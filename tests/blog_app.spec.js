aa495c86dee0f6c6542624560f01a5fc32698c4b
Jan Keronen <keronen.jan@gmail.com>
2024-05-02 11:35:04 +0300

    add e2e tests done

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,9 +1,9 @@
-const { loginWith, createBlog } = require('./testhelper')
+const { loginWith, createBlog, logout } = require('./testhelper')
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
+    // before each test
     await request.post('http://localhost:3003/api/testing/reset')
     await request.post('http://localhost:3003/api/users', {
       data:{
@@ -12,27 +12,27 @@ describe('Blog app', () => {
         password: 'salainen'
       }      
     })
-
+    await request.post('http://localhost:3003/api/users', {
+      data:{
+        name: 'Ismo Laitela',
+        username: 'ilaitela',
+        password: 'salainen'
+      }      
+    })
     await page.goto('http://localhost:5173')
   })
-
   test('Login button is shown', async ({ page }) => {
-    
     await expect (page.getByRole('button', {name: 'log in'})).toBeVisible()
-
   })
   describe('Login', () => {
     beforeEach(async ({ page, request }) => {
-
+      // Nothing to do here
      })
     test('succeeds with correct credentials', async ({ page }) => {
-      
       await loginWith(page, 'staalasma', 'salainen')
       await expect(page.getByText('Seppo Taalasmaa is logged in')).toBeVisible()
-     
     })
     test('succeeds with wrong credentials', async ({ page }) => {
-      
       await loginWith(page, 'staalasma', 'wrongpassword')
       await expect(page.getByText('Seppo Taalasmaa is logged in')).not.toBeVisible()
     })
@@ -41,14 +41,53 @@ describe('Blog app', () => {
     beforeEach(async ({ page }) => {
       await loginWith(page, 'staalasma', 'salainen')
     })
-  
     test('a new blog can be created', async ({ page }) => {
       await createBlog(page, 'Title for testing', 'Url for testing')
       await createBlog(page, 'Another title for testing', 'Second Url for testing')
-      expect(await page.getByRole('heading', { name: 'Title: Title for testing' })).toBeVisible()
-      //await locator('form').filter({ hasText: 'Title: Title for testing' }).toBeVisible()
-      //await expect (page.getByText('Title: Title for testingview')).toBeVisible()
-      
+      await expect(page.getByRole('heading', { name: 'Title: Title for testing' })).toBeVisible()
+    })
+    describe('Blog', () => {
+      beforeEach(async ({ page }) => {
+        await createBlog(page, 'Title for testing', 'Url for testing')
+        await createBlog(page, 'Another title for testing', 'Second Url for testing')
+      })
+      test('like-button increases likes', async ({ page }) => {
+        const blogDiv = page.getByText('Title: Another title for testing').locator('..') 
+        await blogDiv.getByRole('button', { name: 'view'}).click();
+        await page.getByRole('button', { name: 'like'}).click()
+        await page.waitForTimeout(200);
+        expect(page.getByText('Likes: 1')).toBeVisible();
+      })
+      test('"Remove Blog" removes the blog', async ({ page }) => {
+        const blogDiv = page.getByText('Title: Another title for testing').locator('..') 
+        await blogDiv.getByRole('button', { name: 'view'}).click();
+        
+        await page.waitForTimeout(200);
+        page.on('dialog', async dialog => {
+          expect(dialog.type()).toContain('confirm');
+          expect(dialog.message()).toContain('Do you really want to remove blog?');
+          await dialog.accept();
+        })
+        await page.getByRole('button', { name: 'Delete Blog'}).click();
+        await page.waitForTimeout(200);
+        expect(page.getByText('Title: Another title for testing')).not.toBeVisible();
+      })
+      test('If user not a owner, "Remove Blog"- button do not appear', async ({ page }) => {
+        await logout(page);
+        await expect(page.getByRole('button', {name: 'log in'})).toBeVisible();
+        await loginWith(page, 'ilaitela', 'salainen')
+        await expect(page.getByText('Ismo Laitela is logged in')).toBeVisible();
+        const blogDiv = page.getByText('Title: Another title for testing').locator('..') 
+        await blogDiv.getByRole('button', { name: 'view'}).click();
+        await expect(page.getByRole('button', { name: 'Delete Blog'})).not.toBeVisible();
+      })
+      test('Blogs are sorted by likes, most liked first', async ({ page }) => {
+        await expect(page.getByRole('form').nth(1).getByRole('heading', { name: 'Title: Another title for testing'})).toBeVisible();
+        await page.locator('form').filter({ hasText: 'Title: Another title for testing' }).getByRole('button', {name: 'view'}).click();
+        await page.getByRole('button', { name: 'like'}).click();
+        await page.waitForTimeout(200)
+        await expect(page.getByRole('form').nth(0).getByRole('heading', { name: 'Title: Another title for testing'})).toBeVisible();
+      })
     })
   })
 })
--- tests/testhelper.js
+++ tests/testhelper.js
@@ -1,15 +1,18 @@
 const loginWith = async (page, username, password)  => {
-    await page.getByRole('button', { name: 'log in' }).click()
-    await page.getByRole('textbox').first().fill(username)
-    await page.getByRole('textbox').last().fill(password)
-    await page.getByRole('button', { name: 'login' }).click()
-  }
-  
-  const createBlog = async (page, title,url) => {
-    await page.getByRole('button', { name: 'add new blog' }).click()
-    await page.getByRole('textbox').first().fill(title)
-    await page.getByRole('textbox').last().fill(url)
-    await page.getByRole('button', { name: 'add' }).click()
-  }
-  
-  export { loginWith, createBlog }
+  await page.getByRole('button', { name: 'log in' }).click()
+  await page.getByRole('textbox').first().fill(username)
+  await page.getByRole('textbox').last().fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+const createBlog = async (page, title,url) => {
+  await page.getByRole('button', { name: 'add new blog' }).click()
+  await page.getByRole('textbox').first().fill(title)
+  await page.getByRole('textbox').last().fill(url)
+  await page.getByRole('button', { name: 'add' }).click()
+  // wait untill title appears to page
+  await page.getByRole('heading' ,{name: title}).waitFor();
+}
+const logout = async (page)  => {
+  await page.getByRole('button', { name: 'logout' }).click()
+}
+export { loginWith, createBlog, logout }
